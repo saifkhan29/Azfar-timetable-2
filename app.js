af3f7e9c2cfc270dce55abd5349ea77e9179d6e5
saifkhan29 <79800224+saifkhan29@users.noreply.github.com>
2022-02-16 12:48:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,274 @@
+const Pab = document.querySelectorAll('.pab1')
+
+Pab.forEach(a_head => {
+    a_head.innerText = "PAB DAAOA Theory"
+    a_head.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+const Pab_b1_tut = document.querySelectorAll('.pab_b1_tut')
+
+Pab_b1_tut.forEach(a_head => {
+    a_head.innerText = "PAB DAAOA:B1 Tutorial"
+    a_head.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+const Pab_b2_tut = document.querySelectorAll('.pab_b2_tut')
+
+Pab_b2_tut.forEach(a_head => {
+    a_head.innerText = "PAB DAAOA:B2 Tutorial"
+    a_head.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+const Pab_b3_tut = document.querySelectorAll('.pab_b3_tut')
+
+Pab_b3_tut.forEach(a_head => {
+    a_head.innerText = "PAB DAAOA:B3 Tutorial"
+    a_head.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+//////////////////////////////
+
+const ppg = document.querySelectorAll('.ppg1')
+
+ppg.forEach(ppg2 => {
+    ppg2.innerText = "PPG MLADL Theory"
+    ppg2.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+////////////////////////////////
+
+
+const rsj = document.querySelectorAll('.rsj1')
+
+rsj.forEach(rsj2 => {
+    rsj2.innerText = 'RSJ MLADL:B1 Tutorial';
+    rsj2.href = 'https://meet.google.com/yko-bysx-ksm'
+})
+
+const rsj3 = document.querySelectorAll('.rsj3')
+
+rsj3.forEach(rsj2 => {
+    rsj2.innerText = 'RSJ MLADL:B3 Tutorial';
+    rsj2.href = 'https://meet.google.com/yko-bysx-ksm'
+})
+
+const rsj_b2_tut = document.querySelectorAll('.rsj_b2_tut')
+
+rsj_b2_tut.forEach(rsj2 => {
+    rsj2.innerText = 'RSJ MLADL:B2 Tutorial';
+    rsj2.href = 'https://meet.google.com/yko-bysx-ksm'
+})
+
+
+////////////////////////////////
+
+const psd = document.querySelectorAll('.psd1')
+
+psd.forEach(psd2 => {
+    psd2.innerText = 'PSD WTACC:B3 Tutorial'
+    psd2.href = 'https://meet.google.com/vsi-duid-upf'
+})
+
+const psd_b2_tut = document.querySelectorAll('.psd_b2_tut')
+
+psd_b2_tut.forEach(psd2 => {
+    psd2.innerText = 'PSD WTACC:B2 Tutorial'
+    psd2.href = 'https://meet.google.com/vsi-duid-upf'
+})
+
+///////////////////////////////
+
+const pabB2 = document.querySelectorAll('.pabB2')
+
+pabB2.forEach(pab2 => {
+    pab2.innerText = 'PAB DAAOA:B2 Tutorial'
+    pab2.href = 'https://meet.google.com/apr-rqcy-wnt'
+})
+
+////////////////////////
+
+const ars1 = document.querySelectorAll('.ars1')
+
+ars1.forEach(ars => {
+    ars.innerText = 'ARS WTACC Theory'
+    ars.href = 'https://meet.google.com/cez-ztbd-vdn'
+})
+
+
+///////////////////////////////
+
+const djj1 = document.querySelectorAll('.djj1')
+
+djj1.forEach(djj => {
+    djj.innerText = 'DJJ SP Theory'
+    djj.href = 'https://meet.google.com/azd-rpcy-stn'
+})
+
+///////////////////////////////
+
+const djj_b1_lab = document.querySelectorAll('.djj_b1_lab')
+const djj_b2_lab = document.querySelectorAll('.djj_b2_lab')
+const djj_b3_lab = document.querySelectorAll('.djj_b3_lab')
+
+djj_b1_lab.forEach(lab => {
+    lab.innerText = 'DJJ SP:B1 Lab'
+    lab.href = 'https://meet.google.com/azd-rpcy-stn'
+})
+
+djj_b2_lab.forEach(lab => {
+    lab.innerText = 'DJJ SP:B2 Lab'
+    lab.href = 'https://meet.google.com/azd-rpcy-stn'
+})
+
+djj_b3_lab.forEach(lab => {
+    lab.innerText = 'DJJ SP:B3 Lab'
+    lab.href = 'https://meet.google.com/azd-rpcy-stn'
+})
+
+///////////////////////////////
+
+const prk_b1_lab = document.querySelectorAll('.prk_b1_lab')
+const prk_b2_lab = document.querySelectorAll('.prk_b2_lab')
+const prk_b3_lab = document.querySelectorAll('.prk_b3_lab')
+
+prk_b1_lab.forEach(lab => {
+    lab.innerText = 'PRK WTACC:B1 Lab'
+    lab.href = 'https://meet.google.com/uer-fwau-ywy'
+})
+
+prk_b2_lab.forEach(lab => {
+    lab.innerText = 'PRK WTACC:B2 Lab'
+    lab.href = 'https://meet.google.com/uer-fwau-ywy'
+})
+
+prk_b3_lab.forEach(lab => {
+    lab.innerText = 'PRK WTACC:B3 Lab'
+    lab.href = 'https://meet.google.com/uer-fwau-ywy'
+})
+
+///////////////////////////////
+
+const pab_b1_lab = document.querySelectorAll('.pab_b1_lab')
+const pab_b2_lab = document.querySelectorAll('.pab_b2_lab')
+const pab_b3_lab = document.querySelectorAll('.pab_b3_lab')
+
+pab_b1_lab.forEach(lab => {
+    lab.innerText = 'PAB DAAOA:B1 Lab'
+    lab.href = 'https://meet.google.com/apr-rqcy-wnt'
+})
+
+pab_b2_lab.forEach(lab => {
+    lab.innerText = 'PAB DAAOA:B2 Lab'
+    lab.href = 'https://meet.google.com/apr-rqcy-wnt'
+})
+
+pab_b3_lab.forEach(lab => {
+    lab.innerText = 'PAB DAAOA:B3 Lab'
+    lab.href = 'https://meet.google.com/apr-rqcy-wnt'
+})
+
+///////////////////////////////
+
+const ppg_b1_lab = document.querySelectorAll('.ppg_b1_lab')
+const ppg_b2_lab = document.querySelectorAll('.ppg_b2_lab')
+const ppg_b3_lab = document.querySelectorAll('.ppg_b3_lab')
+
+ppg_b1_lab.forEach(lab => {
+    lab.innerText = 'PPG MLADL:B1 Lab'
+    lab.href = 'http://meet.google.com/pjc-dckf-vka'
+})
+
+ppg_b2_lab.forEach(lab => {
+    lab.innerText = 'PPG MLADL:B2 Lab'
+    lab.href = 'http://meet.google.com/pjc-dckf-vka'
+})
+
+ppg_b3_lab.forEach(lab => {
+    lab.innerText = 'PPG MLADL:B3 Lab'
+    lab.href = 'http://meet.google.com/pjc-dckf-vka'
+})
+
+///////////////////////////////
+
+const ars_b1_lab = document.querySelectorAll('.ars_b1_lab')
+const ars_b2_lab = document.querySelectorAll('.ars_b2_lab')
+const ars_b3_lab = document.querySelectorAll('.ars_b3_lab')
+
+ars_b1_lab.forEach(lab => {
+    lab.innerText = 'ARS WTACC:B2 Lab'
+    lab.href = 'https://meet.google.com/cez-ztbd-vdn'
+})
+
+ars_b2_lab.forEach(lab => {
+    lab.innerText = 'ARS WTACC:B2 Lab'
+    lab.href = 'https://meet.google.com/cez-ztbd-vdn'
+})
+
+ars_b3_lab.forEach(lab => {
+    lab.innerText = 'ARS WTACC:B3 Lab'
+    lab.href = 'https://meet.google.com/cez-ztbd-vdn'
+})
+
+/////////////text animation///////////
+
+document.addEventListener('DOMContentLoaded',function(event){
+    // array with texts to type in typewriter
+    var dataText = ["WELCOME", "HAVE A NICE DAY", "SY-IT TIME TABLE"];
+    
+    // type one text in the typwriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+      // chekc if text isn't finished yet
+      if (i < (text.length)) {
+        // add next character to h1
+       document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+  
+        // wait for a while and call this function again for next character
+        setTimeout(function() {
+          typeWriter(text, i + 1, fnCallback)
+        }, 100);
+      }
+      // text finished, call callback if there is a callback function
+      else if (typeof fnCallback == 'function') {
+        // call callback after timeout
+        setTimeout(fnCallback, 700);
+      }
+    }
+    // start a typewriter animation for a text in the dataText array
+     function StartTextAnimation(i) {
+       if (typeof dataText[i] == 'undefined'){
+          setTimeout(function() {
+            StartTextAnimation(0);
+          }, 20000);
+       }
+       // check if dataText[i] exists
+      if (i < dataText[i].length) {
+        // text exists! start typewriter animation
+       typeWriter(dataText[i], 0, function(){
+         // after callback (and whole text has been animated), start next text
+         StartTextAnimation(i + 1);
+       });
+      }
+    }
+    // start the text animation
+    StartTextAnimation(0);
+});
+
+////////////bg color///////////////
+
+// particle.min.js hosted on GitHub
+// Scroll down for initialisation code
+
+!function(a){var b="object"==typeof self&&self.self===self&&self||"object"==typeof global&&global.global===global&&global;"function"==typeof define&&define.amd?define(["exports"],function(c){b.ParticleNetwork=a(b,c)}):"object"==typeof module&&module.exports?module.exports=a(b,{}):b.ParticleNetwork=a(b,{})}(function(a,b){var c=function(a){this.canvas=a.canvas,this.g=a.g,this.particleColor=a.options.particleColor,this.x=Math.random()*this.canvas.width,this.y=Math.random()*this.canvas.height,this.velocity={x:(Math.random()-.5)*a.options.velocity,y:(Math.random()-.5)*a.options.velocity}};return c.prototype.update=function(){(this.x>this.canvas.width+20||this.x<-20)&&(this.velocity.x=-this.velocity.x),(this.y>this.canvas.height+20||this.y<-20)&&(this.velocity.y=-this.velocity.y),this.x+=this.velocity.x,this.y+=this.velocity.y},c.prototype.h=function(){this.g.beginPath(),this.g.fillStyle=this.particleColor,this.g.globalAlpha=.7,this.g.arc(this.x,this.y,1.5,0,2*Math.PI),this.g.fill()},b=function(a,b){this.i=a,this.i.size={width:this.i.offsetWidth,height:this.i.offsetHeight},b=void 0!==b?b:{},this.options={particleColor:void 0!==b.particleColor?b.particleColor:"#fff",background:void 0!==b.background?b.background:"#1a252f",interactive:void 0!==b.interactive?b.interactive:!0,velocity:this.setVelocity(b.speed),density:this.j(b.density)},this.init()},b.prototype.init=function(){if(this.k=document.createElement("div"),this.i.appendChild(this.k),this.l(this.k,{position:"absolute",top:0,left:0,bottom:0,right:0,"z-index":1}),/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(this.options.background))this.l(this.k,{background:this.options.background});else{if(!/\.(gif|jpg|jpeg|tiff|png)$/i.test(this.options.background))return console.error("Please specify a valid background image or hexadecimal color"),!1;this.l(this.k,{background:'url("'+this.options.background+'") no-repeat center',"background-size":"cover"})}if(!/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(this.options.particleColor))return console.error("Please specify a valid particleColor hexadecimal color"),!1;this.canvas=document.createElement("canvas"),this.i.appendChild(this.canvas),this.g=this.canvas.getContext("2d"),this.canvas.width=this.i.size.width,this.canvas.height=this.i.size.height,this.l(this.i,{position:"relative"}),this.l(this.canvas,{"z-index":"20",position:"relative"}),window.addEventListener("resize",function(){return this.i.offsetWidth===this.i.size.width&&this.i.offsetHeight===this.i.size.height?!1:(this.canvas.width=this.i.size.width=this.i.offsetWidth,this.canvas.height=this.i.size.height=this.i.offsetHeight,clearTimeout(this.m),void(this.m=setTimeout(function(){this.o=[];for(var a=0;a<this.canvas.width*this.canvas.height/this.options.density;a++)this.o.push(new c(this));this.options.interactive&&this.o.push(this.p),requestAnimationFrame(this.update.bind(this))}.bind(this),500)))}.bind(this)),this.o=[];for(var a=0;a<this.canvas.width*this.canvas.height/this.options.density;a++)this.o.push(new c(this));this.options.interactive&&(this.p=new c(this),this.p.velocity={x:0,y:0},this.o.push(this.p),this.canvas.addEventListener("mousemove",function(a){this.p.x=a.clientX-this.canvas.offsetLeft,this.p.y=a.clientY-this.canvas.offsetTop}.bind(this)),this.canvas.addEventListener("mouseup",function(a){this.p.velocity={x:(Math.random()-.5)*this.options.velocity,y:(Math.random()-.5)*this.options.velocity},this.p=new c(this),this.p.velocity={x:0,y:0},this.o.push(this.p)}.bind(this))),requestAnimationFrame(this.update.bind(this))},b.prototype.update=function(){this.g.clearRect(0,0,this.canvas.width,this.canvas.height),this.g.globalAlpha=1;for(var a=0;a<this.o.length;a++){this.o[a].update(),this.o[a].h();for(var b=this.o.length-1;b>a;b--){var c=Math.sqrt(Math.pow(this.o[a].x-this.o[b].x,2)+Math.pow(this.o[a].y-this.o[b].y,2));c>120||(this.g.beginPath(),this.g.strokeStyle=this.options.particleColor,this.g.globalAlpha=(120-c)/120,this.g.lineWidth=.7,this.g.moveTo(this.o[a].x,this.o[a].y),this.g.lineTo(this.o[b].x,this.o[b].y),this.g.stroke())}}0!==this.options.velocity&&requestAnimationFrame(this.update.bind(this))},b.prototype.setVelocity=function(a){return"fast"===a?1:"slow"===a?.33:"none"===a?0:.66},b.prototype.j=function(a){return"high"===a?5e3:"low"===a?2e4:isNaN(parseInt(a,10))?1e4:a},b.prototype.l=function(a,b){for(var c in b)a.style[c]=b[c]},b});
+
+// Initialisation
+
+var canvasDiv = document.getElementById('particle-canvas');
+var options = {
+  particleColor: '#888',
+  background: 'https://raw.githubusercontent.com/JulianLaval/canvas-particle-network/master/img/demo-bg.jpg',
+  interactive: true,
+  speed: 'medium',
+  density: 'high'
+};
+var particleCanvas = new ParticleNetwork(canvasDiv, options);
