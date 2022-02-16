c33210d9f6b78b91726dc8225ceea9292963a86f
saifkhan29 <79800224+saifkhan29@users.noreply.github.com>
2022-02-16 12:54:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -239,7 +239,7 @@ document.addEventListener('DOMContentLoaded',function(event){
        if (typeof dataText[i] == 'undefined'){
           setTimeout(function() {
             StartTextAnimation(0);
-          }, 20000);
+          }, 1000);
        }
        // check if dataText[i] exists
       if (i < dataText[i].length) {
